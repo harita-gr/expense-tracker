753dd98c24636eb404232d42e6705b91b6754a77
Harita Ravindranath <haritagr19@gmail.com>
2024-04-11 22:14:20 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Balance from "./components/Balance";
+import Transactions from "./components/Transactions";
+import NewTransaction from "./components/NewTransaction";
+import ExpenseSheet from "./components/ExpenseSheet";
+import { Typography, Box } from "@mui/material";
 
 function App() {
+  const [transactions, setTransactions] = useState([
+    { id: 1, text: "Books", amount: -20 },
+    { id: 2, text: "Flowers", amount: -10 },
+    { id: 3, text: "Salary", amount: 2000 },
+    { id: 4, text: "Groceries", amount: -40 },
+  ]);
+
+  const addTransactions = (transaction) => {
+    setTransactions((transactions) => [transactions, ...transaction]);
+  };
+
+  const deleteTransactions = (id) => {
+    setTransactions(
+      transactions.filter((transaction) => transaction.id !== id)
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typography mb="20px">Expense Tracker</Typography>
+      <Box>
+        <Balance transactions={transactions} />
+        <ExpenseSheet transactions={transactions} />
+        <Transactions
+          transactions={transactions}
+          deleteTransactions={deleteTransactions}
+        />
+        <NewTransaction addTransactions={addTransactions} />
+      </Box>
     </div>
   );
 }
---
+++ src/components/ExpenseSheet.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ExpenseSheet = () => {
+  return <div>ExpenseSheet</div>;
+};
+
+export default ExpenseSheet;
